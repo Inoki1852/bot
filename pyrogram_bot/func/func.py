2e5b70dc0d22c2a9bad878e940fc3313c10b9fab
Inoki1852 <44931712+Inoki1852@users.noreply.github.com>
2022-01-14 12:48:15 +0200

    Add files via upload

---
+++ pyrogram_bot/func/func.py
@@ -0,0 +1,21 @@
+from pyrogram import Client
+from pyrogram.raw import base, functions
+import datetime
+import time
+import asyncio
+from pyrogram.errors import RPCError
+
+app = Client("my_account")
+
+async def cur_time(app):
+  async with app:
+    while True:
+        fullUser = await app.send(functions.users.GetFullUser(id = await app.resolve_peer("inoki1852")))
+        now = time.strftime("My time is: %H:%M", time.localtime())
+        if fullUser.full_user.about == now:
+            await asyncio.sleep(5)
+        else:
+            await app.update_profile(bio = now)
+          
+app.run(cur_time(app))
+
