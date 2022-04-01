2923de06439bb6ce1616d4d6582ad38c719eb103
Danil Opryatov <44931712+Inoki1852@users.noreply.github.com>
2022-04-01 13:51:15 +0300

    Add files via upload

---
+++ pyrogram_bot/plugins/info.py
@@ -0,0 +1,23 @@
+from pyrogram import Client, filters
+
+from chat_reply import cr
+
+
+@Client.on_message(filters.me & filters.regex(r'^\.idm$'))
+def idm(app, message):
+    chat, reply = cr(app, message)
+    if reply is None:
+        app.send_message(chat.id, message, reply_to_message_id=message.message_id)
+    else:
+        app.send_message(chat.id, reply, reply_to_message_id=message.message_id)
+
+
+@Client.on_message(filters.me & filters.regex(r'^\.user$'))
+def copy(app, message):
+    chat, reply = cr(app, message)
+    if reply is None:
+        user_id = message.from_user.id
+    else:
+        user_id = reply.from_user.id
+    user = app.get_users(user_id)
+    app.send_message(chat.id, user, reply_to_message_id=message.message_id)
