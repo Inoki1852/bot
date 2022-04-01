37c0383662b5b29c7200be625451e1f5aaa767e4
Danil Opryatov <44931712+Inoki1852@users.noreply.github.com>
2022-04-01 18:03:21 +0300

    Update mention.py

--- pyrogram_bot/plugins/mention.py
+++ pyrogram_bot/plugins/mention.py
@@ -7,9 +7,6 @@ from chat_reply import cr
 @Client.on_message(filters.me & filters.regex(r'^\.mt'))
 def mention(app, message):
     chat, reply = cr(app, message)
-    text = re.findall(r'@?(\w+) (\w+)', message.text)
+    text = re.findall(r'\.mt @?(\w+) ((\w+|\W+ ?)+)', message.text)
     user = app.get_users(text[0][0])
-    print(text[0][1])
-    print(user.id)
-    print(user)
     app.edit_message_text(chat.id, message.message_id, "[" + text[0][1] + "](tg://user?id=" + str(user.id) + ")")
