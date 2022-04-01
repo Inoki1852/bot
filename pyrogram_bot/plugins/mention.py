eee7ffd8bf13cdc701f7f63bbdd7b170e0e29a94
Danil Opryatov <44931712+Inoki1852@users.noreply.github.com>
2022-04-01 16:01:17 +0300

    Add files via upload

--- pyrogram_bot/plugins/mention.py
+++ pyrogram_bot/plugins/mention.py
@@ -1,12 +1,15 @@
 from pyrogram import Client, filters
-from pyrogram.handlers import MessageHandler
 
 import re
 from chat_reply import cr
 
 
-@Client.on_message(filters.me & filters.regex(r'^\.m'))
+@Client.on_message(filters.me & filters.regex(r'^\.mt'))
 def mention(app, message):
     chat, reply = cr(app, message)
-
-
+    text = re.findall(r'@?(\w+) (\w+)', message.text)
+    user = app.get_users(text[0][0])
+    print(text[0][1])
+    print(user.id)
+    print(user)
+    app.edit_message_text(chat.id, message.message_id, "[" + text[0][1] + "](tg://user?id=" + str(user.id) + ")")
