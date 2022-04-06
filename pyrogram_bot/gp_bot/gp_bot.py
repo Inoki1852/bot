32d5f99e0173ed64389c5f57657a56f23d5dd02b
Danil Opryatov <44931712+Inoki1852@users.noreply.github.com>
2022-04-06 21:02:14 +0300

    Update gp_bot.py

--- pyrogram_bot/gp_bot/gp_bot.py
+++ pyrogram_bot/gp_bot/gp_bot.py
@@ -1,5 +1,6 @@
 from pyrogram import Client
 from pyrogram import filters
+import re
 
 app = Client("my_bot", bot_token="5005315425:AAGc9fI1PumWim7YehHlh5xaMjJ2W2g5DN4")
 
@@ -18,7 +19,7 @@ def suggest(client, message):
             app.send_message(value, user_message)
 
 
-@app.on_message(filters.private & (
+@app.on_message(filters.private & filters.regex(r'^[^\.].+') & (
         filters.user("inoki1852") | filters.user("ClayzDart") | filters.user("MrBuxlo") | filters.user("Nne_li")))
 def media(client, message):
     reply = message.reply_to_message
@@ -31,5 +32,21 @@ def media(client, message):
                        caption="{}  <code>by {}</code>".format(message.text, message.from_user.username))
             message.reply("Успешно скопировано!")
 
+
+@app.on_message(filters.private & filters.regex(r'^\.') & (
+        filters.user("inoki1852") | filters.user("ClayzDart") | filters.user("MrBuxlo") | filters.user("Nne_li")))
+def answer(client, message):
+    reply = message.reply_to_message
+    if reply is not None:
+        media = reply.media
+        if media is None:
+            user_name = re.findall(r'by (\w+)', reply.text)
+        else:
+            user_name = re.findall(r'by (\w+)', reply.caption)
+        text_answer = re.findall(r'\.(.*)', message.text)
+        try:
+            app.send_message(user_name[0], text_answer[0])
+        except:
+            pass
 
 app.run()
