6f952aae4b07dbe6ed37f111619effe208d9a4f1
Danil Opryatov <44931712+Inoki1852@users.noreply.github.com>
2022-04-08 08:44:00 +0300

    Update gp_bot.py

--- pyrogram_bot/gp_bot/gp_bot.py
+++ pyrogram_bot/gp_bot/gp_bot.py
@@ -6,46 +6,48 @@ app = Client("my_bot", bot_token="5005315425:AAGc9fI1PumWim7YehHlh5xaMjJ2W2g5DN4
 
 users = ["inoki1852", "ClayzDart", "Nne_li"]
 
-@app.on_message(filters.private)
+
+@app.on_message(filters.private & filters.regex(r'^[^#].+'))
 def suggest(client, message):
+    text = re.findall(r'^\.?(.+)', message.text)
     media = message.media
     if media is not None:
         for value in users:
-            message.copy(value, caption="{}  <code>by {}</code>".format(message.text, message.from_user.username))
+            message.copy(value, caption="{}  <code>by {}</code>".format(text[0], message.from_user.username))
     else:
-        user_message = "{}  <code>by {}</code>".format(message.text, message.from_user.username)
+        user_message = "{}:\n— {}".format(message.from_user.username, text[0])
         for value in users:
             app.send_message(value, user_message)
 
 
-@app.on_message(filters.private & filters.regex(r'^[^\.].+') & (
+@app.on_message(filters.private & filters.reply & filters.text & filters.regex(r'^#.+') & (
         filters.user("inoki1852") | filters.user("ClayzDart") | filters.user("MrBuxlo") | filters.user("Nne_li")))
 def media(client, message):
     reply = message.reply_to_message
-    if reply is None:
-        pass
-    else:
-        media = reply.media
-        if media is not None:
-            reply.copy("gifs_and_pics",
-                       caption="{}  <code>by {}</code>".format(message.text, message.from_user.username))
-            message.reply("Успешно скопировано!")
+    media = reply.media
+    if media is not None:
+        reply.copy("gifs_and_pics",
+                   caption="{}  <code>by {}</code>".format(message.text, message.from_user.username))
+        message.reply("Успешно скопировано!")
 
 
-@app.on_message(filters.private & filters.regex(r'^\.') & (
+@app.on_message(filters.private & filters.reply & filters.regex(r'^\.') & (
         filters.user("inoki1852") | filters.user("ClayzDart") | filters.user("Nne_li")))
 def answer(client, message):
     reply = message.reply_to_message
-    if reply is not None:
-        media = reply.media
-        if media is None:
-            user_name = re.findall(r'by (\w+)', reply.text)
-        else:
-            user_name = re.findall(r'by (\w+)', reply.caption)
-        text_answer = re.findall(r'\.(.*)', message.text)
-        try:
-            app.send_message(user_name[0], "{}  <code>by {}</code>".format(text_answer[0], message.from_user.username))
-        except:
-            pass
+    media = reply.media
+    if media is None:
+        user_name = re.findall(r'by (\w+)', reply.text)
+    else:
+        user_name = re.findall(r'by (\w+)', reply.caption)
+    text_answer = re.findall(r'\.(.*)', message.text)
+    print(text_answer[0])
+    if text_answer[0] == "":
+        text_answer[0] = ""
+    try:
+        app.send_message(user_name[0], "<i>{}</i>:\n— {}".format(text_answer[0], message.from_user.username))
+    except:
+        pass
+
 
 app.run()
