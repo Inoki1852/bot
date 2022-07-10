12df423df68a6fd04162a378710c0497d3cd301e
Danil Opryatov <44931712+Inoki1852@users.noreply.github.com>
2022-07-10 21:50:25 +0300

    Update main.py

--- pyrogram_bot/main.py
+++ pyrogram_bot/main.py
@@ -1,5 +1,7 @@
 from pyrogram import Client
 
-app = Client("my_account", 1474699, "5427b333bbe9380419f6fdf2ccdd37a2")
+plugins = dict(root="plugins")
+
+app = Client("my_account", 1474699, "5427b333bbe9380419f6fdf2ccdd37a2", plugins=plugins)
 
 app.run()
